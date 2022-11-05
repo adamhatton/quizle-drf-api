ec5ad87e58bce2aa341162aec8366eb0628a93eb
adamhatton <adamdhatton@gmail.com>
2022-11-05 14:26:47 +0000

    Add environment variable for secret key

--- eny.py
+++
@@ -1,3 +0,0 @@
-import os
-
-os.environ['CLOUDINARY_URL'] = 'cloudinary://633671374127829:nG2DaK7hppxLv0WcZLlhlDyfn-4@det1cakel'
--- quizle/settings.py
+++ quizle/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
-if os.path.exists('eny.py'):
+if os.path.exists('env.py'):
     import env
 
 CLOUDINNARY_STORAGE = {
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q^6=s4sc$v@ea98rvg)pk_cdkcx5$(=#*x=i##rsg0-o7ghv8n'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
